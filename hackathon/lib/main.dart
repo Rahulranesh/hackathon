577cec9195a6623f58b44b5c3937d0545005570b
QuickSlot <quickslot@hackathon.dev>
2026-06-10 20:43:06 +0530

    feat: wire MultiProvider in main.dart with injected repositories

--- hackathon/lib/main.dart
+++ hackathon/lib/main.dart
@@ -1,29 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'providers/app_state.dart';
+import 'data/services/api_service.dart';
+import 'data/repositories/venue_repository.dart';
+import 'data/repositories/booking_repository.dart';
+import 'presentation/providers/user_provider.dart';
+import 'presentation/providers/venue_provider.dart';
+import 'presentation/providers/slot_provider.dart';
+import 'presentation/providers/booking_provider.dart';
+import 'presentation/screens/login_screen.dart';
 import 'theme/app_theme.dart';
-import 'screens/home_screen.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  final api = ApiService();
+  final venueRepo = VenueRepository(api);
+  final bookingRepo = BookingRepository(api);
+
   runApp(
     MultiProvider(
-      providers: [ChangeNotifierProvider(create: (_) => AppState())],
-      child: const HackathonApp(),
+      providers: [
+        ChangeNotifierProvider(create: (_) => UserProvider()),
+        ChangeNotifierProvider(create: (_) => VenueProvider(venueRepo)),
+        ChangeNotifierProvider(create: (_) => SlotProvider(venueRepo)),
+        ChangeNotifierProvider(create: (_) => BookingProvider(bookingRepo)),
+      ],
+      child: const QuickSlotApp(),
     ),
   );
 }
 
-class HackathonApp extends StatelessWidget {
-  const HackathonApp({super.key});
+class QuickSlotApp extends StatelessWidget {
+  const QuickSlotApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Hackathon Project',
-      debugShowCheckedModeBanner: false,
-      theme: AppTheme.lightTheme,
-      home: const HomeScreen(),
-    );
-  }
+  Widget build(BuildContext context) => MaterialApp(
+        title: 'QuickSlot',
+        debugShowCheckedModeBanner: false,
+        theme: AppTheme.dark,
+        home: const LoginScreen(),
+      );
 }
